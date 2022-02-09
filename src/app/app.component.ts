30658e569143847d4a8230410beec14a81f7149c
rahman <rahman@datumcorp.com>
2022-02-09 23:02:25 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Aleph-Labs';
+export class AppComponent implements OnInit {
+  minDate: Date;
+  maxDate: Date;
+
+  form: FormGroup
+  constructor(translate: TranslateService, http: HttpClient, private _formBuilder: FormBuilder,
+  ) {
+
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang()
+    translate.use(browserLang);
+    // laod from api
+    const arabicFile$ = http.get('assets/fake-api/dummyAr.json');
+    const englishFile$ = http.get('assets/fake-api/dummyEn.json');
+
+
+    // mock api merging files to translation fille
+    forkJoin([
+      arabicFile$,
+      englishFile$,
+      translate.getTranslation(browserLang),
+    ]).subscribe(res => {
+      let arabicFile = res[0];
+      let englishFile = res[1];
+      let fileTomerge =
+        translate.currentLang === 'en'
+          ? englishFile
+          : arabicFile;
+      translate.setTranslation(
+        'en',
+        {
+          ...fileTomerge
+        },
+        true
+      );
+      console.log(arabicFile, englishFile)
+    })
+
+
+  }
+  ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 20, 0, 1);
+    this.maxDate = new Date(currentYear + 1, 11, 31);
+    this.form = this._formBuilder.group({
+      name: [
+        null,
+        Validators.compose([
+          Validators.required,
+
+        ]),
+      ],
+      date: [
+        null,
+        Validators.compose([
+          Validators.required,
+        ]),
+      ]
+
+    });
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MatNativeDateModule } from '@angular/material/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ReactiveFormsModule,
+    FormsModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
